5685eb0ee851c5c0038ede50054fd3ec222556c4
Antony Lodge <71594095+lodgeant@users.noreply.github.com>
2022-01-10 20:47:08 +0000

    Updates to code

--- ASCII/source.c
+++ ASCII/source.c
@@ -1,10 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <string.h>
-#include "utils.h"
 
-#define COLUMN_WIDTH 4
+#define MAX_COLUMNS 5
+#define MIN_COLUMNS 1
 
 
 // ** PROTOTYPES **
@@ -15,18 +14,34 @@ int IntToBinary(int k);
 int main()
 {
 
-
-	// ** Run process over and over **
 	while (1)
-	{		
+	{
 		// ** Get start and end values from user **	
-		int startIndex;// = 48;
-		int endIndex;// = 58;
+		int startIndex = 0;		// = 48;
+		int endIndex = 0;		// = 58;
+		int columnWidth = 0;
+
+		// ** Get Column Width from user **
+		while (columnWidth != -1)
+		{
+			printf("Enter Column Width: ");
+			scanf("%d", &columnWidth);
+			if (columnWidth == -1) break;			
+			else if (columnWidth >= MIN_COLUMNS && columnWidth <= MAX_COLUMNS) break;
+			else if (columnWidth < MIN_COLUMNS || columnWidth > MAX_COLUMNS)
+			{
+				printf("Column width must be between %d and %d\n\n", MIN_COLUMNS, MAX_COLUMNS);
+			}			
+		}
+		if (columnWidth == -1) break;
+
+		// ** Get start and end values from user **			
 		printf("Enter Start Index: ");
-		scanf("%d", &startIndex);		
+		scanf("%d", &startIndex);
 		if (startIndex == -1) break;
+
 		printf("Enter End Index: ");
-		scanf("%d", &endIndex);		
+		scanf("%d", &endIndex);
 		if (endIndex == -1) break;
 
 		// ** Print ASCII details for range using user provided details **	
@@ -35,20 +50,30 @@ int main()
 		for (int a = startIndex; a <= endIndex; a++)
 		{
 			printf("%03d %08d %c\t\t", a, IntToBinary(a), a);
-			if (i % COLUMN_WIDTH == 0) printf("\n");
+			if (i % columnWidth == 0) printf("\n");
 			i++;
 		}
 
 		// ** Add padding **
 		printf("\n\n\n");
 	}
-
-		
-
-
+	
+	
 	// ** Finish Up **
 	printf("\n\n\n");
 	printf("Function completed...Press any key to continue\n");
 	//getchar();
 	return 0;
 }
+
+
+
+int IntToBinary(int k)
+{
+	if (k == 0) return 0;
+	if (k == 1) return 1;                       /* optional */
+	return (k % 2) + 10 * IntToBinary(k / 2);
+}
+
+
+
--- ASCII/utils.h
+++
@@ -1,7 +0,0 @@
-
-int IntToBinary(int k)
-{
-	if (k == 0) return 0;
-	if (k == 1) return 1;                       /* optional */
-	return (k % 2) + 10 * IntToBinary(k / 2);
-}
