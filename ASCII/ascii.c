d46e9c6ed5d9a32195f26a2e097395c02095677b
Antony Lodge <71594095+lodgeant@users.noreply.github.com>
2022-01-11 20:42:10 +0000

    Resolved #1 | Clear screen for all new requests

--- ASCII/ascii.c
+++ ASCII/ascii.c
@@ -3,12 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// ** DEFINED VARIABLES **
 #define VERSION "1.1"
 #define CREATED_BY "Antony Lodge"
 #define MAX_COLUMNS 5
 #define MIN_COLUMNS 1
 #define MIN_INDEX 0
 #define MAX_INDEX 500
+
+// ** GLOBAL VARIABLES **
+const char* CLEAR_SCREEN_ANSI = "@cls||clear";
 
 
 // ** PROTOTYPES **
@@ -17,77 +22,46 @@ void ShowVersion();
 int GetColumnWidth();
 int GetStartIndex();
 int GetEndIndex(int);
+void PrintASCIIList(int, int, int);
+int CheckForTryAgain();
 
 
 // ** MAIN **
 int main()
 {
-	// ** Show application details **
-	ShowVersion();
-
-
-	// ** Start application **
+	// ** Start application loop **
 	while (1)
 	{
 		// ** Clear screen **		
-		//system("@cls||clear");
+		system(CLEAR_SCREEN_ANSI);
 
-		// ** Get start and end values from user **	
-		//int startIndex = 0;		// = 48;
-		//int endIndex = 0;		// = 58;
-		//int columnWidth = 0;
+		// ** Show application details **
+		ShowVersion();
+
+		// ** Init variables **	
+		int startIndex = 0;		// = 48;
+		int endIndex = 0;		// = 58;
+		int columnWidth = 0;
 
 		// ** Get Column Width from user **
-		int columnWidth = GetColumnWidth();
+		columnWidth = GetColumnWidth();
 		if (columnWidth == -1) break;
 
 		// ** Get start and end values from user **
-		int startIndex = GetStartIndex();
+		startIndex = GetStartIndex();
 		if (startIndex == -1) break;
-		int endIndex = GetEndIndex(startIndex);
+		endIndex = GetEndIndex(startIndex);
 		if (endIndex == -1) break;
 
-
-		
-
-
-
-
-
-		// ** Print ASCII details for range using user provided details **	
-		printf("\n\n");
-		int i = 1;
-		for (int a = startIndex; a <= endIndex; a++)
-		{
-			//printf("%03d %08d %c\t\t", a, IntToBinary(a), a);
-			if (a == 10)
-			{
-				printf("%03d %08d #\t\t", a, IntToBinary(a));
-			}
-			else
-			{
-				printf("%03d %08d %c\t\t", a, IntToBinary(a), a);
-			}			
-			if (i % columnWidth == 0) printf("\n");
-			i++;
-		}
+		// ** Print ASCII details for range using user provided details **
+		PrintASCIIList(columnWidth, startIndex, endIndex);
 
 		// ** Add padding **
 		printf("\n\n\n");
 
-
-		// ** Check if another run is required **
-		//char response;
-		//printf("Run again...?");
-		//if (scanf("%c", &response) != 1)
-		//{
-		//	// ERROR NEEDS TO BE CAUGHT HERE...
-		//}
-		//if (response != 'y' && response != 'Y')
-		//{
-		//	break;
-		//}
-
+		// ** Check if another run is required **		
+		int tryAgain = CheckForTryAgain();
+		if (tryAgain == 0) break;
 	}
 	
 	
@@ -106,7 +80,6 @@ int IntToBinary(int k)
 	if (k == 1) return 1;                       /* optional */
 	return (k % 2) + 10 * IntToBinary(k / 2);
 }
-
 
 void ShowVersion()
 {
@@ -185,4 +158,54 @@ int GetEndIndex(int startIndex)
 	}
 	return endIndex;
 }
+
+void PrintASCIIList(int columnWidth, int startIndex, int endIndex)
+{
+	// ** Print ASCII details for range using user provided details **	
+	printf("\n\n");
+	int i = 1;
+	for (int a = startIndex; a <= endIndex; a++)
+	{
+		//printf("%03d %08d %c\t\t", a, IntToBinary(a), a);
+		if (a == 10)
+		{
+			printf("%03d %08d #\t\t", a, IntToBinary(a));
+		}
+		else
+		{
+			printf("%03d %08d %c\t\t", a, IntToBinary(a), a);
+		}
+		if (i % columnWidth == 0) printf("\n");
+		i++;
+	}
+}
+
+int CheckForTryAgain()
+{
+	int tryAgain = 1;
+	printf("\nRun again...?");
+	while (tryAgain == 1)
+	{
+		char response;
+		if (scanf("%c", &response) != 1)
+		{
+			// ERROR NEEDS TO BE CAUGHT HERE...
+		}
+		else if (response == '\n')
+		{
+		}
+		else if (response != 'y' && response != 'Y')
+		{
+			tryAgain = 0;
+			break;
+		}
+		else if (response == 'y' || response == 'Y')
+		{
+			tryAgain = 1;
+			break;
+		}
+	}
+	return tryAgain;
+}
+
 
