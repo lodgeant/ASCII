f6fde749c6a8fef173f44116b5b01e4a3fc827e5
Antony Lodge <71594095+lodgeant@users.noreply.github.com>
2022-01-11 20:02:46 +0000

    Moved getting colum width and start/end index to own methods

--- ASCII/ascii.c
+++ ASCII/ascii.c
@@ -3,15 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VERSION "1.0"
+#define VERSION "1.1"
 #define CREATED_BY "Antony Lodge"
 #define MAX_COLUMNS 5
 #define MIN_COLUMNS 1
+#define MIN_INDEX 0
+#define MAX_INDEX 500
 
 
 // ** PROTOTYPES **
 int IntToBinary(int k);
 void ShowVersion();
+int GetColumnWidth();
+int GetSTartIndex();
 
 
 
@@ -28,45 +32,26 @@ int main()
 		// ** Clear screen **		
 		//system("@cls||clear");
 
-		
-
-
 		// ** Get start and end values from user **	
-		int startIndex = 0;		// = 48;
-		int endIndex = 0;		// = 58;
-		int columnWidth = 0;
+		//int startIndex = 0;		// = 48;
+		//int endIndex = 0;		// = 58;
+		//int columnWidth = 0;
 
 		// ** Get Column Width from user **
-		while (columnWidth != -1)
-		{
-			printf("Enter Column Width: ");
-			if (scanf("%d", &columnWidth) != 1)
-			{
-				// ERROR NEEDS TO BE CAUGHT HERE...
-			}
-			if (columnWidth == -1) break;			
-			else if (columnWidth >= MIN_COLUMNS && columnWidth <= MAX_COLUMNS) break;
-			else if (columnWidth < MIN_COLUMNS || columnWidth > MAX_COLUMNS)
-			{
-				printf("Column width must be between %d and %d\n\n", MIN_COLUMNS, MAX_COLUMNS);
-			}			
-		}
+		int columnWidth = GetColumnWidth();
 		if (columnWidth == -1) break;
 
-		// ** Get start and end values from user **			
-		printf("Enter Start Index: ");		
-		if (scanf("%d", &startIndex) != 1)
-		{
-			// ERROR NEEDS TO BE CAUGHT HERE...
-		}
+		// ** Get start and end values from user **
+		int startIndex = GetStartIndex();
 		if (startIndex == -1) break;
-
-		printf("Enter End Index: ");		
-		if (scanf("%d", &endIndex) != 1)
-		{
-			// ERROR NEEDS TO BE CAUGHT HERE...
-		}
+		int endIndex = GetEndIndex();
 		if (endIndex == -1) break;
+
+
+
+
+
+
 
 		// ** Print ASCII details for range using user provided details **	
 		printf("\n\n");
@@ -130,4 +115,66 @@ void ShowVersion()
 	printf("\n\n");
 }
 
+int GetColumnWidth()
+{
+	// ** Get Column Width from user **
+	int columnWidth = 0;
+	while (columnWidth != -1)
+	{
+		printf("Enter Column Width: ");
+		if (scanf("%d", &columnWidth) != 1)
+		{
+			// ERROR NEEDS TO BE CAUGHT HERE...
+		}
+		if (columnWidth == -1) break;			
+		else if (columnWidth >= MIN_COLUMNS && columnWidth <= MAX_COLUMNS) break;
+		else if (columnWidth < MIN_COLUMNS || columnWidth > MAX_COLUMNS)
+		{
+			printf("Column width must be between %d and %d\n\n", MIN_COLUMNS, MAX_COLUMNS);
+		}			
+	}
+	return columnWidth;
+}
+
+int GetStartIndex()
+{
+	// ** Get start index from user **		
+	int startIndex = 0;
+	while (startIndex != -1)
+	{
+		printf("Enter Start Index: ");
+		if (scanf("%d", &startIndex) != 1)
+		{
+			// ERROR NEEDS TO BE CAUGHT HERE...
+		}
+		if (startIndex == -1) break;
+		else if (startIndex >= MIN_INDEX && startIndex <= MAX_INDEX) break;
+		else if (startIndex < 0 || startIndex > 500)
+		{
+			printf("Start Index must be between %d and %d\n\n", MIN_INDEX, MAX_INDEX);
+		}
+	}
+	return startIndex;
+}
+
+int GetEndIndex()
+{
+	// ** Get end index from user **		
+	int endIndex = 0;
+	while (endIndex != -1)
+	{
+		printf("Enter End Index: ");
+		if (scanf("%d", &endIndex) != 1)
+		{
+			// ERROR NEEDS TO BE CAUGHT HERE...
+		}
+		if (endIndex == -1) break;
+		else if (endIndex >= MIN_INDEX && endIndex <= MAX_INDEX) break;
+		else if (endIndex < 0 || endIndex > 500)
+		{
+			printf("End Index must be between %d and %d\n\n", MIN_INDEX, MAX_INDEX);
+		}
+	}
+	return endIndex;
+}
 
